accaae9800a44f13b51186d0ade7145a2891bb38
Kuiming <benjamin0901@gmail.com>
2016-02-15 10:37:33 +0800

    renew server.R

--- fibro/server.R
+++ fibro/server.R
@@ -1,30 +1,20 @@
-# By default, the file size limit is 5MB. It can be changed by
-# setting this option. Here we'll raise limit to 9MB.
+
 options(shiny.maxRequestSize = 9*1024^2)
 options(digits = 7)
 shinyServer(function(input, output) {
   
-
-  
   output$contents <- renderDataTable({
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
     cutoff=c(0.09,0.35,0.54)
-    x=data.frame(FibA=as.numeric(input$A),
-                 FibB=as.numeric(input$B),
-                 FibC=as.numeric(input$C),
+    x=data.frame(A=as.numeric(input$A),
+                 B=as.numeric(input$B),
+                 C=as.numeric(input$C),
                  stringsAsFactors = FALSE)
 
-
-   
     if (input$action){
       q()
     }
     
-      x$score=exp(-3.8+2.8*x$FibA-4.01*x$FibB+0.7*x$FibC)/(1+exp(-3.8+2.8*x$FibA-4.01*x$FibB+0.7*x$FibC))
+      x$score=exp(-3.8+2.8*x$A-4.01*x$B+0.7*x$C)/(1+exp(-3.8+2.8*x$A-4.01*x$B+0.7*x$C))
       if (x$score<cutoff[1]){
         x$stage='Health'
       }
